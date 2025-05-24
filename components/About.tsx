65b956821a3aa5e695673e79044a32d434c91076
usamarashedddev <usamarasheed2188@gmail.com>
2025-05-24 22:16:12 +0500

    update about and experiance

--- components/About.tsx
+++ components/About.tsx
@@ -12,17 +12,14 @@ const About = () => {
         <p className=" tracking-[0.5em] text-transparent font-light pb-5  bg-clip-text bg-gradient-to-r from-purple-700 to-orange-500  text-1xl">
           EXPLORE NOW
         </p>
-        <p className="text-gray-300 text-center ">
-          Im a skilled web developer with expertise in building modern, responsive UIs with React, React Native,
-          and CSS frameworks. I craft performant back-ends with Node.js and Express, utilize databases like
-          Firebase and MongoDB, and leverage PWA and offline capabilities for enhanced user experiences.
+        <p className="text-gray-300 text-center">
+          I'm a passionate and results-driven web developer with a strong focus on building modern, responsive user interfaces using React, React Native, and advanced CSS frameworks. I architect high-performance back-end systems with Node.js and Express, and work seamlessly with databases like Firebase and MongoDB to ensure scalable and real-time applications.
 
           <br />
           <br />
-          My
-          proficiency in JavaScript, TypeScript, Git, and GitHub makes me a reliable team player ready to tackle
-          any web development challenge.
+          {/* I also integrate Progressive Web App (PWA) features and offline capabilities to enhance user experience across devices. With a solid foundation in JavaScript and TypeScript, and a collaborative mindset backed by Git and GitHub, I'm a dependable team player who thrives in dynamic environments and is always ready to tackle complex development challenges. */}
         </p>
+
       </div>
     </section>
   );
--- components/Experience.tsx
+++ components/Experience.tsx
@@ -10,20 +10,42 @@ const Experience = () => {
         EXPLORE NOW
       </p>
       <div className=" container mx-auto 2xl ">
-        <div className="flex  md:flex-row  flex-col text-center  justify-center md:justify-between pt-5">
-          <p className="text-gray-300 ">
-            <span className="font-semibold">MERN Stack Developer /</span> Software Engineer
-          </p>
-          <p className="text-gray-300">AUG 2022 - PRESENT</p>
-        </div>
-        <p className="text-gray-300 pt-5 text-justify">
-          Im a skilled web developer with expertise in building modern, responsive UIs with React, React Native,
-          and CSS frameworks. I craft performant back-ends with Node.js and Express, utilize databases like
-          Firebase and MongoDB, and leverage PWA and offline capabilities for enhanced user experiences. My
-          proficiency in JavaScript, TypeScript, React Js, Node Js, Git, and GitHub makes me a reliable team player ready to tackle
-          any web development challenge.
 
-        </p>
+        <div className="bg-gray-800 p-10 rounded-md">
+
+          <div className="flex  md:flex-row  flex-col text-center  justify-center md:justify-between pt-5">
+            <p className="text-gray-300 ">
+              <span className="font-semibold">MERN Stack Developer /</span> Software Engineer
+            </p>
+            <p className="text-gray-300">Dec 2024 - PRESENT</p>
+          </div>
+          <p className="text-gray-300 pt-5 text-justify">
+            As a Full Stack Developer with deep expertise in the MERN stack (MongoDB, Express.js, React.js, Node.js), I specialize in building responsive, scalable web applications with modern front-end and robust back-end architectures. I design and deploy microservice-based systems using AWS (EC2, S3, RDS, Lambda, ECS) and leverage Docker for containerization and orchestration.
+             I automate development workflows through CI/CD pipelines using GitHub Actions. My work emphasizes clean API design, third-party integrations.
+             With strong skills in Git-based collaboration, DevOps, and cloud-native practices, I bridge the gap between development and deployment to deliver reliable, maintainable software systems from concept to production.
+
+          </p>
+        </div>
+        
+        <div className="bg-gray-800 p-10 rounded-md mt-6">
+
+          <div className="flex  md:flex-row  flex-col text-center  justify-center md:justify-between pt-5">
+            <p className="text-gray-300 ">
+              <span className="font-semibold">MERN Stack Developer </span>
+            </p>
+            <p className="text-gray-300">AUG 2022 - Nov 2024</p>
+          </div>
+          <p className="text-gray-300 pt-5 text-justify">
+            Im a skilled web developer with expertise in building modern, responsive UIs with React, React Native,
+            and CSS frameworks. I craft performant back-ends with Node.js and Express, utilize databases like
+            Firebase and MongoDB, and leverage PWA and offline capabilities for enhanced user experiences. My
+            proficiency in JavaScript, TypeScript, React Js, Node Js, Git, and GitHub makes me a reliable team player ready to tackle
+            any web development challenge.
+
+          </p>
+        </div>
+
+
         <div className="flex-col justify-center items-center flex sm:flex-row flex-wrap">
           <div className="bg-transparent  mt-5 mr-2 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
             React
